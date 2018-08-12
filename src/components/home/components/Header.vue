e6a041cf596e575dbb26cca14e5f82d110f0cf22
KaridaLiu <1510128550@qq.com>
2018-08-12 17:05:26 +0800

    header finish 1

--- src/components/home/components/Header.vue
+++ src/components/home/components/Header.vue
@@ -2,7 +2,7 @@
   <div id="header">
     <div class="header-logo">嗡嗡嗡</div>
     <div class="header-search">
-      <input class="header-input" type="text" placeholder="搜索目的地/攻略/游记" />
+      搜索目的地/攻略/游记
       <span class="search-icon iconfont">&#xe651;</span>
     </div>
     <div class="header-login">登录</div>
@@ -33,20 +33,13 @@ export default{
     .header-search
       position: relative
       flex: 1
-      height: .3rem
+      height: .2rem
       background-color: #f3f3f3
       border-radius: .18rem
       margin: .17rem .1rem
       color: #d1d1d1
-      .header-input
-        position: absolute
-        width: 85%
-        height: .2rem
-        background-color: rgba(0, 0, 0, 0)
-        border: none
-        margin: .05rem .15rem
-        color: #d1d1d1
-        font-size: .15rem
+      padding: .05rem .15rem
+      line-height: .2rem
       .search-icon
         position: absolute
         right: .15rem
